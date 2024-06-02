cebad1aa5f32cefa70253e5375571b34594774a2
Flávio Schefer <flavio.schefer@gmail.com>
2024-06-02 11:10:16 -0300

    Learning Variables and string interpolation

--- Program.cs
+++ Program.cs
@@ -5,5 +5,14 @@ class FirstProgram
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello World, I am studying C#");
+        string FirstName;
+        string age;
+        Console.WriteLine("What's your first name?");
+        FirstName = Console.ReadLine();
+        Console.WriteLine("What's your age?");
+        age = Console.ReadLine();
+        //Console.WriteLine("The user name is " + FirstName + " " + LastName);}
+        string interpolatedResponse = $"The user's name is {FirstName} and he's {age} years old";
+        Console.WriteLine(interpolatedResponse);
     }
 }
---
+++ Variables/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number = 1;
+char letter = 'A';
+double dec;
+dec = 1.234;
+bool isHungry = true;
